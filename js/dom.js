34ad8f15aa7885c9ea854f1fe140ddc1af747181
HodeonArtz <martidicianolold@gmail.com>
2024-11-25 18:45:32 +0100

    createSquareDiv implemented

--- js/dom.js
+++ js/dom.js
@@ -1,10 +1,10 @@
 export const exercise1 = document.querySelector(".exercise1 .random-numbers");
 
 /**
- * Crea un div cuadrado.
+ * Generates a new square div.
  *
- * @param {Element} parentElement selecciona dónde va a estar el nuevo div
- * @param {string} textContent contenido que va a tener el div
+ * @param {Element} parentElement indicates where will the squre be generated
+ * @param {string} textContent text that the div will contain
  */
 export function createSquareDiv(parentElement, textContent) {
   const newSquareDiv = document.createElement("div");
