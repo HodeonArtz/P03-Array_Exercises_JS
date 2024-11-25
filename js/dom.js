8eefdb29ff1ee861e7d4bcea993d86363b5e96c0
HodeonArtz <martidicianolold@gmail.com>
2024-11-25 18:44:03 +0100

    createSquareDiv created

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,14 @@
-export const exercise1 = document.querySelector;
+export const exercise1 = document.querySelector(".exercise1 .random-numbers");
 
 /**
+ * Crea un div cuadrado.
  *
- * @param {string} parentSelector
- * @param {string} textContent
+ * @param {Element} parentElement selecciona dónde va a estar el nuevo div
+ * @param {string} textContent contenido que va a tener el div
  */
-export function createSquareDiv(parentSelector, textContent) {}
+export function createSquareDiv(parentElement, textContent) {
+  const newSquareDiv = document.createElement("div");
+  newSquareDiv.classList.add("square");
+  newSquareDiv.textContent = textContent;
+  parentElement.appendChild(newSquareDiv);
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 "use strict";
 
 import * as dom from "./dom";
+
+// >>=====>>====>>====#[<| Exercise 1 |>]#====<<====<<=====<<
+// Generate 5 random numbers on each new div
