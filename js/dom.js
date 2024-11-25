3072c99176c5e475ddea82cf5dd5169b5281a87a
HodeonArtz <martidicianolold@gmail.com>
2024-11-25 18:36:03 +0100

    First commit

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+export const exercise1 = document.querySelector;
+
+/**
+ *
+ * @param {string} parentSelector
+ * @param {string} textContent
+ */
+export function createSquareDiv(parentSelector, textContent) {}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+"use strict";
+
+import * as dom from "./dom";
