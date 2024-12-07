396bcf573d46cd478cb7395354f6e28cc4441353
HodeonArtz <martidicianolold@gmail.com>
2024-12-07 02:03:10 +0100

    feat(disks): implement select disk

--- js/hanoi/main.js
+++ js/hanoi/main.js
@@ -26,11 +26,17 @@ const rods = [
 const rodElements = [...document.querySelectorAll(".hanoi__disks-container")];
 
 /**
+ * @returns {Element|null}
+ */
+let selectedDisk = null;
+
+/**
  *
  * @param {number} diskNumber
+ * @param {function} handleOnClick
  * @returns {Element}
  */
-function generateDisk(diskNumber) {
+function generateDisk(diskNumber, handleOnClick) {
   if (diskNumber < 1 || diskNumber > 5) {
     throw new Error(`Error generating disk with disk number ${diskNumber}`);
   }
@@ -42,6 +48,7 @@ function generateDisk(diskNumber) {
   buttonDisk.dataset.isSelected = false;
   buttonDisk.dataset.diskNum = diskNumber;
   buttonDisk.textContent = diskNumber;
+  buttonDisk.addEventListener("click", handleOnClick);
 
   itemDisk.classList.add("hanoi__disk__container");
   itemDisk.appendChild(buttonDisk);
@@ -67,9 +74,9 @@ function setStartGame(numberOfDisks) {
   });
 
   for (let diskNum = 1; diskNum <= numberOfDisks; diskNum++) {
-    const diskGenerated = generateDisk(diskNum);
+    const diskGenerated = generateDisk(diskNum, handleOnSelectDisk);
     rodElements[0].appendChild(diskGenerated);
-    rods[0].disks.push(diskGenerated);
+    rods[0].disks.push(diskGenerated.querySelector("button"));
   }
 }
 
@@ -99,5 +106,63 @@ function handleOnStartGame(event) {
 }
 
 const gameSettingsForm = document.querySelector(".game__start__settings");
-
 gameSettingsForm.addEventListener("submit", handleOnStartGame);
+
+// <<===========||===========||===========||===========>>
+
+/**
+ * @param {Event|undefined} event
+ */
+function handleOnEndGame(event) {
+  event?.preventDefault();
+  showStartScreen();
+}
+
+const endGameButton = document.querySelector(".game__in-game__btn-end-game");
+endGameButton.addEventListener("click", handleOnEndGame);
+
+// <<===========||===========||===========||===========>>
+
+/**
+ *
+ * @param {Element} disk
+ * @returns {number} 1-3
+ */
+function getRodPosition(disk) {
+  return rods.findIndex((rod) => rod.disks.includes(disk));
+}
+
+function getDiskNumber(disk) {
+  return +disk.dataset.diskNum;
+}
+
+/**
+ * get all numbers from an array of disks
+ * @param {Element[]} rodDisks
+ * @returns {number[]}
+ */
+function getDisksNumbers(rodDisks) {
+  return rodDisks.map((disk) => getDiskNumber(disk));
+}
+
+/**
+ * @param {Event|undefined} event
+ */
+function handleOnSelectDisk(event) {
+  const currentSmallestNumber = getDisksNumbers(
+    rods[getRodPosition(event.target)].disks
+  )[0];
+
+  const clickedNumber = getDiskNumber(event.target);
+
+  if (currentSmallestNumber < clickedNumber) return;
+
+  if (selectedDisk === event.target) {
+    selectedDisk.dataset.isSelected = false;
+    selectedDisk = null;
+    return;
+  }
+  selectedDisk = event.target;
+
+  selectedDisk.dataset.isSelected = true;
+}
